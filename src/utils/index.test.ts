f0c0a67c6d5036b769e099729dc01f22ce26e4ac
QoB <kackar212@gmail.com>
2023-06-30 23:29:34 +0200

    Add tests for getPageCount function

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,3 +1,4 @@
+import { getPageCount } from './get-page-count';
 import { isNumber } from './is-number';
 
 describe('utils', () => {
@@ -5,6 +6,15 @@ describe('utils', () => {
     it('should return true if specified value is of type number', () => {
       expect(isNumber(1)).toBe(true);
       expect(isNumber('1')).toBe(false);
+    });
+  });
+
+  describe('getPageCount', () => {
+    it('should return page count based on total and perPage arguments', () => {
+      expect(getPageCount(100, 25)).toBe(4);
+      expect(getPageCount(50, 6)).toBe(9);
+      expect(getPageCount(0, 25)).toBe(1);
+      expect(getPageCount(0, 0)).toBe(1);
     });
   });
 });
