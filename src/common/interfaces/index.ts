2b1308c9d7c62cff723b35c4ab1aab56d4e11443
QoB <kackar212@gmail.com>
2023-06-30 03:55:21 +0200

    Refactor src/common/interfaces/index.ts to export all interfaces

--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './pagination-item.interface';
 export * from './pagination-page.interface';
 export * from './pagination-gap.interface';
 export * from './use-pagination-base-props.interface';
+export * from './use-pagination-props-with-count.interface';
