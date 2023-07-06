ffb5a23e9a1acdf2ada135c00695a5c319036ee0
QoB <kackar212@gmail.com>
2023-07-06 23:59:53 +0200

    Add tests for useConfig hook

---
+++ src/hooks/use-config/use-config.test.ts
@@ -0,0 +1,30 @@
+import { renderHook } from '@testing-library/react';
+import { defaults } from '@common';
+import { useConfig } from './use-config.hook';
+
+describe('useConfig', () => {
+  it('should return normalized and merged config', () => {
+    const { result: config } = renderHook(() =>
+      useConfig({
+        pageCount: 10,
+        boundaryCount: 2,
+        siblingCount: { start: 1, end: 2 },
+      })
+    );
+
+    const { result } = renderHook(() => useConfig({ total: 10, perPage: 5 }));
+
+    expect(config.current).toEqual({
+      ...defaults,
+      pageCount: 10,
+      boundaryCount: { start: 2, end: 2 },
+      siblingCount: { start: 1, end: 2 },
+    });
+
+    expect(result.current).toEqual({
+      ...defaults,
+      total: 10,
+      perPage: 5,
+    });
+  });
+});
