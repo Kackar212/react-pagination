6bd324a61461b19650708ab7306ea4d6666ed11f
QoB <kackar212@gmail.com>
2023-07-25 18:30:51 +0200

    Update jest config file, it will now use single quotes to match whole project

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 /** @type {import('jest').Config} */
 const config = {
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  testEnvironment: "jsdom",
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
-    "^@components$": "<rootDir>/src/components/index.ts",
-    "^@hooks$": "<rootDir>/src/hooks/index.ts",
-    "^@utils$": "<rootDir>/src/utils/index.ts",
-    "^@common$": "<rootDir>/src/common/index.ts",
-    "^@components/(.*)$": "<rootDir>/src/components/$1",
-    "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
-    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
-    "^@common/(.*)$": "<rootDir>/src/common/$1",
-    "\\.(css|scss)$": "identity-obj-proxy",
+    '^@components$': '<rootDir>/src/components/index.ts',
+    '^@hooks$': '<rootDir>/src/hooks/index.ts',
+    '^@utils$': '<rootDir>/src/utils/index.ts',
+    '^@common$': '<rootDir>/src/common/index.ts',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@common/(.*)$': '<rootDir>/src/common/$1',
+    '\\.(css|scss)$': 'identity-obj-proxy',
   },
 };
 
