4a596a03e5b4917f3cb92eea1da34f60ab956c34
QoB <kackar212@gmail.com>
2023-07-25 15:43:17 +0200

    Update PaginationItemType to add types for first, previous, next and last page

--- src/common/enums/pagination-item-type.enum.ts
+++ src/common/enums/pagination-item-type.enum.ts
@@ -2,4 +2,8 @@ export enum PaginationItemType {
   StartGap = 'start-gap',
   EndGap = 'end-gap',
   Page = 'page',
+  PreviousPage = 'previous page',
+  NextPage = 'next page',
+  FirstPage = 'first page',
+  LastPage = 'last page',
 }
