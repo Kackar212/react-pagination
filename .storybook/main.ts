36e07ee404a848f5c12588c8f08ab4157717e21f
QoB <kackar212@gmail.com>
2023-07-25 18:32:30 +0200

    Update storybook config files, they will now use single quotes to match whole project

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,26 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+import type { StorybookConfig } from '@storybook/react-webpack5';
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     {
-      name: "@storybook/addon-styling",
+      name: '@storybook/addon-styling',
       options: {
         sass: {
-          implementation: require("node-sass"),
+          implementation: require('node-sass'),
         },
       },
     },
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
   webpackFinal: async (config, { configType }) => {
     if (!config.resolve) return config;
