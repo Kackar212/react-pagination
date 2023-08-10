4aff9ad1066242700bf1aec8d1e3625c9adceb2d
Kackar212 <kackar212@gmail.com>
2023-08-10 21:14:59 +0200

    Update rollup config to add support for svgr

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import postcss from 'rollup-plugin-postcss';
+import svgr from '@svgr/rollup';
 
 const packageJson = require('./package.json');
 
@@ -29,6 +30,7 @@ export default [
         tsconfig: './tsconfig.json',
         exclude: ['src/**/*.test.tsx', 'src/**/*.stories.tsx'],
       }),
+      svgr(),
       postcss({
         modules: true,
       }),
