346f3baabd375b84ca4cf8379327c4cab7bfb149
bazaltowy <frontieronfront@protonmail.com>
2023-08-10 20:41:53 +0200

    Update storybook config in order to add support for importing svg as a component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -23,9 +23,25 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   webpackFinal: async (config, { configType }) => {
-    if (!config.resolve) return config;
+    if (!config.resolve || !config.module) return config;
 
     config.resolve.plugins = [new TsconfigPathsPlugin()];
+
+    const imageRule = config.module.rules?.find((rule) => {
+      if (typeof rule !== 'string' && rule.test instanceof RegExp) {
+        return rule.test.test('.svg');
+      }
+    });
+
+    if (typeof imageRule !== 'string' && imageRule) {
+      imageRule.exclude = /\.svg$/;
+    }
+
+    config.module.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
     return config;
   },
 };
